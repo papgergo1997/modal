23c1cb09ac4968267edf19f7443466895bcdadbf
Pap Gergő <pap.gergo.1997@gmail.com>
2020-11-15 11:33:17 +0100

    modal-size-modified

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@
 let modal = document.getElementById("modal") ;
 let modalBtn = document.getElementById("openModal") ;
 let closeBtn = document.getElementById("close") ;
+let close2Btn = document.getElementById("close2");
+
 
 modalBtn.onclick = function() {
-    modal.style.display = "block"
+    modal.style.display = "block";
+    modal.classList.add("showModal");
 };
 
 closeBtn.onclick = function() {
     modal.style.display = "none";
-  };
+};
+
+close2Btn.onclick = function() {
+    modal.style.display = "none";
+};
 
 window.onclick = function(event) {
     if (event.target == modal) {
